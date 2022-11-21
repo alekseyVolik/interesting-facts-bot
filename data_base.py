e848d853502a3b653d60445f43f6b4c230c83402
Aleksey_Volik <volikjob@gmail.com>
2022-11-21 16:43:38 +0400

    Add SQLAlchemy ORM model
    Add TelegramChat SQLAlchemy ORM model that save info about chat ID (this ID use for send the 'facts-message') and unable/able receive messages

---
+++ data_base.py
@@ -0,0 +1,16 @@
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import (
+    Column,
+    Integer, String, Boolean
+)
+
+
+Base = declarative_base()
+
+
+class TelegramChat(Base):
+    __table_name__ = "telegram_chat"
+
+    _id = Column(Integer, primary_key=True)
+    chat_id = Column(String(20))
+    receive_fact = Column(Boolean)
